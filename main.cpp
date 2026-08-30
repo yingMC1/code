06eedfb87b51a63854e97395c606e63e6e19dd42
yingMC-1 <19198189385@163.com>
2026-08-30 22:07:10 +0800

    重构代码，优化树结构，更新查询和更新逻辑

--- main.cpp
+++ main.cpp
@@ -1,68 +1,85 @@
 #include <bits/stdc++.h>
 
+#define int long long
 #define endl '\n'
 using namespace std;
-using i64 = long long;
-const int N = 5e5 + 5;
-const int INF = 2e9;
+using i64   = long long;
+const int N = 1e5 + 5;
 
-int n, q, a[N];
+i64 n, m, a[N], tree[N << 2], tag[N << 2];
 
-struct I {
-    int sum, l, r, len;
-} tree[N << 2];
-
-I push_up(I l, I r) {
-    I res;
-    res.len = l.len + r.len;
-    res.sum = l.sum;
-    if (l.sum == l.len)
-        res.sum += r.sum;
-    res.l = r.l;
-    if (r.l == r.len)
-        res.l += l.l;
-    res.r = max({l.r, r.r, l.l + r.sum});
-    return res;
+void down_sum(int x, int l, int r, int w) {
+    tree[x] += w * (r - l + 1);
+    tag[x] += w;
 }
 
+void down(int x, int l, int r) {
+    if(!tag[x])
+        return;
+    int mid = l + r >> 1;
+    down_sum(x * 2, l, mid, tag[x]);
+    down_sum(x * 2 + 1, mid + 1, r, tag[x]);
+    tag[x] = 0;
+}
+
+void push_up(int x) { tree[x] = tree[N * 2] + tree[N * 2 + 1]; }
+
 void build(int x, int l, int r) {
-    if (l == r) {
-        tree[x].len = 1;
-        tree[x].sum = tree[x].l = tree[x].r = a[l];
+    if(l == r) {
+        tree[x] += a[l];
         return;
     }
     int mid = l + r >> 1;
     build(x * 2, l, mid);
     build(x * 2 + 1, mid + 1, r);
-    tree[x] = push_up(tree[x * 2], tree[x * 2 + 1]);
+    push_up(x);
 }
 
-I query(int x, int l, int r, int ql, int qr) {
-    if (ql <= l && r <= qr) {
-        return tree[x];
+void update(int x, int l, int r, int ql, int qr, int w) {
+    if(ql <= l && r <= qr) {
+        tree[x] += w * (r - l + 1);
+        tag[x] += w;
+        return;
     }
+    down(x, l, r);
     int mid = l + r >> 1;
-    if (qr <= mid)
-        return query(x * 2, l, mid, ql, qr);
-    if (ql > mid)
-        return query(x * 2 + 1, mid + 1, r, ql, qr);
-    I l2 = query(x * 2, l, mid, ql, qr);
-    I r2 = query(x * 2 + 1, mid + 1, r, ql, qr);
-    return push_up(l2, r2);
+    if(ql <= mid)
+        update(x * 2, l, mid, ql, qr, w);
+    if(qr > mid)
+        update(x * 2 + 1, mid + 1, r, ql, qr, w);
+    push_up(x);
 }
 
-int main() {
+int query(int x, int l, int r, int ql, int qr) {
+    if(ql <= l && r <= qr)
+        return tree[x];
+    down(x, l, r);
+    int mid = l + r >> 1, res = 0;
+    if(ql <= mid)
+        res += query(x * 2, l, mid, ql, qr);
+    if(qr > mid)
+        res += query(x * 2 + 1, mid + 1, r, ql, qr);
+    return res;
+}
+
+signed main() {
     ios::sync_with_stdio(false);
     cout.tie(nullptr);
     cin.tie(nullptr);
-    cin >> n;
-    for (int i = 1; i <= n; i++) cin >> a[i];
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
     build(1, 1, n);
-    cin >> q;
-    for (int i = 1; i <= q; i++) {
-        int l, r;
-        cin >> l >> r;
-        cout << query(1, 1, n, l, r).r << endl;
+    for(int i = 1; i <= m; i++) {
+        char c;
+        int  l, r;
+        cin >> c >> l >> r;
+        if(c == 'C') {
+            int x;
+            cin >> x;
+            update(1, 1, n, l, r, x);
+        } else
+            cout << query(1, 1, n, l, r) << endl;
     }
     return 0;
 }
