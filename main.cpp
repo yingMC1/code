99df1c2d3e46e86a79867bc52356aefaf8d8e411
yingMC-1 <19198189385@163.com>
2026-08-31 20:21:46 +0800

    vscode

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
-#define endl '\n'
+#define int long long
 using namespace std;
-using i64 = long long;
+const int N = 1e5 + 5;
 
-int n;
+int t;
 
-int main(){
-    
+struct I {
+    int x, id;
+} b[N];
+
+bool cmp(I x, I y) {
+    if(x.x == y.x)
+        return x.id < y.id;
+    else
+        return x.x < y.x;
+}
+signed main() {
+    cin >> t;
+    for(int i = 1; i <= t; i++) {
+        int n, x;
+        cin >> n >> x;
+        int a[N * 2];
+        for(int i = 1; i <= n; i++) {
+            cin >> a[i];
+            b[i].x  = ceil(a[i] * 1.0 / x * 1.0);
+            b[i].id = i;
+        }
+        sort(b + 1, b + 1 + n, cmp);
+        cout << "Case #" << i << ": ";
+        for(int i = 1; i <= n; i++)
+            .cout << b[i].id << ' ';
+        cout << endl;
+    }
     return 0;
 }
