f9f41ee2ae02e1079e8660ad9e5f34aec65d01ee
yingMC-1 <19198189385@163.com>
2026-08-31 20:51:34 +0800

    更新 C++ 编译任务配置，添加静态链接参数；重构主程序，增加输入输出逻辑

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 
+#define endl '\n'
 using namespace std;
 using i64 = long long;
 
-int main() { return 0; }
+int n, m;
+
+int main() {
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) {
+        if(i % 2) {
+            for(int j = 1; j <= m; j++)
+                cout << (i - 1) * m + j << " ";
+            cout << endl;
+        } else {
+            for(int j = m; j; j--)
+                cout << (i - 1) * m + j << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
