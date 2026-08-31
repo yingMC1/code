300491c596d14c3a131c414ca92848835b03d1af
yingMC-1 <19198189385@163.com>
2026-08-31 21:26:57 +0800

    更新 .clang-format 配置，调整空格和对齐规则；新增 C++ 模板文件；重构 main.cpp，增加输入逻辑和条件判断

---
+++ c++模板.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+using i64 = long long;
+const int N = 1e5 + 5;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -3,21 +3,33 @@
 #define endl '\n'
 using namespace std;
 using i64 = long long;
+const int N = 2e3 + 5;
 
 int n, m;
+string x;
+i64 sum;
+bool f, t1[N][N];
 
 int main() {
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++) {
-        if(i % 2) {
-            for(int j = 1; j <= m; j++)
-                cout << (i - 1) * m + j << " ";
-            cout << endl;
-        } else {
-            for(int j = m; j; j--)
-                cout << (i - 1) * m + j << " ";
-            cout << endl;
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
+    cin >> n >> m >> x;
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            string t;
+            cin >> t;
+            if (t == x)
+                t1[i][j] = true;
+            else if (t == "L")
+                f = true;
         }
-    }
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            int t;
+            cin >> t;
+            if (t1[i][j] || f) sum += t;
+        }
+    cout << sum;
     return 0;
 }
