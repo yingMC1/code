dcd778506a7c42fce9a1d590e5a40cc435e1a3a3
yingMC-1 <19198189385@163.com>
2026-09-03 22:45:52 +0800

    重构 main.cpp，调整变量命名和数据结构；更新运行.bat 脚本；修复 main.exe 二进制差异

--- main.cpp
+++ main.cpp
@@ -5,17 +5,19 @@ using namespace std;
 using i64 = long long;
 const int N = 105;
 
-int n, m, t, dp[N * 2][2];
-
-struct I {
-    int m, t;
-} a[N];
+int n, M, T, m[N], t[N], dp[N * 2][2];
 
 int main() {
-    cin >> n >> m >> t;
+    cin >> n >> M >> T;
     for (int i = 1; i <= n; i++)
-        cin >> a[i].m >> a[i].t;
+        cin >> m[i] >> t[i];
     for (int i = 1; i <= n; i++) {
-        dp[i][1] =
+        for (int j = M; j >= m[i]; j--) {
+            if (dp[j - m[i]][0] + t[i] <= T) {
+                dp[j][0] = max(dp[j][0], dp[j - m[i]][0] + t[i]);
+                dp[j][1] = max(dp[j][1], dp[j - m[i]][1] + 1);
+            }
+        }
     }
+    cout << dp[M][1] << endl;
 }
