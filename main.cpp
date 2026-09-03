b50d902c9d069c3b60245e414a166b11710f1c7d
yingMC-1 <19198189385@163.com>
2026-09-03 22:32:21 +0800

    更新编译任务配置，新增 g++.exe 刷题专用任务；重构 main.cpp，调整数据结构；更新 运行.bat 脚本，简化命令

--- main.cpp
+++ main.cpp
@@ -1,59 +1,21 @@
 #include <bits/stdc++.h>
 
-#define int long long
 #define endl '\n'
 using namespace std;
 using i64 = long long;
-const int N = 100005;
+const int N = 105;
 
-i64 n, m, a[N], tag1[N << 2], tag2[N << 2];
+int n, m, t, dp[N * 2][2];
 
-void push_down(int x, int l, int r) {
-    if (!tag1[x] && !tag2[x]) return;
-    int mid = l + r >> 1;
-    tag1[x * 2] += tag1[x];
-    tag2[x * 2] += tag2[x];
-    tag1[x * 2 + 1] += tag1[x] + (mid - l + 1) * tag2[x];
-    tag2[x * 2 + 1] += tag2[x];
-    tag1[x] = tag2[x] = 0;
-}
+struct I {
+    int m, t;
+} a[N];
 
-void update(int x, int l, int r, int ql, int qr, int k, int d) {
-    if (ql <= l && r <= qr) {
-        tag1[x] += k + (l - ql) * d;
-        tag2[x] += d;
-        return;
-    }
-    push_down(x, l, r);
-    int mid = l + r >> 1;
-    if (ql <= mid) update(x * 2, l, mid, ql, qr, k, d);
-    if (qr > mid) update(x * 2 + 1, mid + 1, r, ql, qr, k, d);
-}
-
-i64 query(int x, int l, int r, int q) {
-    if (l == r) return tag1[x] + a[l];
-    push_down(x, l, r);
-    int mid = l + r >> 1;
-    if (q <= mid)
-        return query(x * 2, l, mid, q);
-    else
-        return query(x * 2 + 1, mid + 1, r, q);
-}
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cout.tie(nullptr);
-    cin.tie(nullptr);
-    cin >> n >> m;
+int main() {
+    cin >> n >> m >> t;
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
-    int op, l, r, k, d;
-    while (m--) {
-        cin >> op >> l;
-        if (op == 1) {
-            cin >> r >> k >> d;
-            update(1, 1, n, l, r, k, d);
-        } else
-            cout << query(1, 1, n, l) << endl;
+        cin >> a[i].m >> a[i].t;
+    for (int i = 1; i <= n; i++) {
+        dp[i][1] =
     }
 }
--- 运行.bat
+++ 运行.bat
@@ -1,3 +1,3 @@
 g++.exe -O2 -g D:\code\main.cpp
 D:\code\a.exe
-cmd /c pause
+pause
