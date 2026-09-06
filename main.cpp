cd7baa070d930996db9b061bca24bca4a7f05e6f
yingMC-1 <19198189385@163.com>
2026-09-06 22:21:16 +0800

    重构 main.cpp，移除冗余代码；优化线段树的更新和查询逻辑

--- main.cpp
+++ main.cpp
@@ -1,99 +1,95 @@
 #include <bits/stdc++.h>
-
-#define endl '\n'
+#define int long long
 using namespace std;
-using i64 = long long;
 const int mod = 998244353;
-const int N = 1e5 + 5;
-
+const int N = 3e5 + 5;
+struct I {
+    int l;
+    int r;
+    int sum;
+    int sum2;
+} tree[4 * N];
 int n, m;
-double tag[N << 2], tree[N << 2], sum[N << 2], a[N];
-
-void push_up(int id) {
-    tree[id] = tree[id * 2] + tree[id * 2 + 1];
-    sum[id] = sum[id * 2] + sum[id * 2 + 1];
+int w[N];
+int ksm(int a, int b, int id) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (res * a) % id;
+        a = a * a % id;
+        b >>= 1;
+    }
+    return res;
 }
 
-void down(int id, int l, int r, double w) {
-    double len = r - l + 1;
-    sum[id] += 2 * w * tree[id] + len * w * w;
-    tree[id] += w * len;
-    tag[id] += w;
+void pushup(int id) {
+    tree[id].sum = (tree[id * 2].sum + tree[id * 2 + 1].sum) % mod;
+    tree[id].sum2 = (tree[id * 2].sum2 + tree[id * 2 + 1].sum2) % mod;
 }
 
-void push_down(int id, int l, int r) {
-    if (!tag[id]) return;
-    int mid = l + r >> 1;
-    down(id * 2, l, mid, tag[id]);
-    down(id * 2 + 1, mid + 1, r, tag[id]);
-    tag[id] = 0;
+void update(int id, int l, int r, int k) {
+    if (l <= tree[id].l && tree[id].r <= r) {
+        tree[id].sum = k % mod;
+        tree[id].sum2 = (k % mod) * (k % mod);
+        return;
+    }
+    int m = tree[id].l + tree[id].r >> 1;
+    if (l <= m) update(id * 2, l, r, k);
+    if (r > m) update(id * 2 + 1, l, r, k);
+    pushup(id);
+}
+
+pair<int, int> query(int id, int l, int r) {
+    int res = 0, res2 = 0;
+    if (l <= tree[id].l && tree[id].r <= r) {
+        return {tree[id].sum, tree[id].sum2};
+    }
+    int m = tree[id].l + tree[id].r >> 1;
+    if (l <= m) {
+        auto [x, s2] = query(id * 2, l, r);
+        res = (res + x) % mod;
+        res2 = (res2 + s2) % mod;
+    }
+    if (r > m) {
+        auto [x, s2] = query(id * 2 + 1, l, r);
+        res = (res + x) % mod;
+        res2 = (res2 + s2) % mod;
+    }
+    return {res, res2};
 }
 
 void build(int id, int l, int r) {
-    if (l == r) {
-        tree[id] = a[l];
-        sum[id] = a[l] * a[l];
-        return;
-    }
-    int mid = l + r >> 1;
-    build(id * 2, l, mid);
-    build(id * 2 + 1, mid + 1, r);
-    push_up(id);
+    tree[id] = {l, r, w[l] % mod, (w[l] % mod) * (w[l] % mod)};
+    if (l == r) return;
+    int m = l + r >> 1;
+    build(id * 2, l, m);
+    build(id * 2 + 1, m + 1, r);
+    pushup(id);
 }
 
-void update(int id, int l, int r, int ql, int qr, double w) {
-    if (ql <= l && r <= qr) {
-        down(id, l, r, w);
-        return;
-    }
-    push_down(id, l, r);
-    int mid = l + r >> 1;
-    if (ql <= mid) update(id * 2, l, mid, ql, qr, w);
-    if (qr > mid) update(id * 2 + 1, mid + 1, r, ql, qr, w);
-    push_up(id);
-}
-
-double query1(int id, int l, int r, int ql, int qr) {
-    if (ql <= l && r <= qr) return tree[id];
-    push_down(id, l, r);
-    int mid = l + r >> 1;
-    double res = 0;
-    if (ql <= mid) res += query1(id * 2, l, mid, ql, qr);
-    if (qr > mid) res += query1(id * 2 + 1, mid + 1, r, ql, qr);
-    return res;
-}
-
-double query2(int id, int l, int r, int ql, int qr) {
-    if (ql <= l && r <= qr) return sum[id];
-    push_down(id, l, r);
-    int mid = l + r >> 1;
-    double res = 0;
-    if (ql <= mid) res += query2(id * 2, l, mid, ql, qr);
-    if (qr > mid) res += query2(id * 2 + 1, mid + 1, r, ql, qr);
-    return res;
-}
-
-int main() {
+signed main() {
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
+        cin >> w[i];
     build(1, 1, n);
-    int op;
-    double l, r, k;
-    for (int i = 1; i <= m; i++) {
-        cin >> op >> l >> r;
+    while (m--) {
+        int op;
+        cin >> op;
         if (op == 1) {
-            cin >> k;
-            update(1, 1, n, l, r, k);
+            int l, r;
+            cin >> l >> r;
+            int len = r - l + 1;
+            int x = query(1, l, r).first;
+            int s2 = query(1, l, r).second;
+            int avg = (x % mod) * ksm(len, mod - 2, mod) % mod;
+            int var = (s2 - ((x * x % mod) * ksm(len, mod - 2, mod)) % mod + mod) % mod;
+            cout << avg << " " << var << "\n";
         } else if (op == 2) {
-            double len = r - l + 1;
-            double ans = query1(1, 1, n, l, r) / len;
-            printf("%.4lf\n", ans);
-        } else {
-            double len = r - l + 1;
-            double res = query1(1, 1, n, l, r) / len;
-            double ans = query2(1, 1, n, l, r) / len - res * res;
-            printf("%.4lf\n", ans);
+            int k, l;
+            cin >> k >> l;
+            update(1, k, k, l);
         }
     }
     return 0;
