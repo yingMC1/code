9268a185c54578c5212975ab582da987b78ee3c1
yingMC-1 <19198189385@163.com>
2026-09-08 20:35:18 +0800

    重构 main.cpp，优化变量声明和逻辑；更新编译任务以使用相对路径

--- main.cpp
+++ main.cpp
@@ -1,69 +1,74 @@
 #include <bits/stdc++.h>
-#define int long long
+
+#define endl '\n'
 using namespace std;
-const int mod = 998244353;
-const int N = 3e5 + 5;
+const int mod = 1000000007;
+const int N = 1e5 + 5;
+
 struct I {
-    int l;
-    int r;
     int sum;
     int sum2;
 } tree[4 * N];
-int n, m;
-int w[N];
+int n, m, b[N];
+
 int ksm(int a, int b, int id) {
-    int res = 1;
+    long long res1 = 1;
     while (b) {
-        if (b & 1) res = (res * a) % id;
-        a = a * a % id;
+        if (b & 1) res1 = res1 * a % id;
+        a = 1LL * a * a % id;
         b >>= 1;
     }
-    return res;
+    return (int)res1;
 }
 
-void pushup(int id) {
+void up(int id) {
     tree[id].sum = (tree[id * 2].sum + tree[id * 2 + 1].sum) % mod;
     tree[id].sum2 = (tree[id * 2].sum2 + tree[id * 2 + 1].sum2) % mod;
 }
 
-void update(int id, int l, int r, int k) {
-    if (l <= tree[id].l && tree[id].r <= r) {
-        tree[id].sum = k % mod;
-        tree[id].sum2 = (k % mod) * (k % mod);
+void build(int x, int l, int r) {
+    if (l == r) {
+        tree[x].sum = b[l];
+        tree[x].sum2 = 1LL * b[l] * b[l] % mod;
         return;
     }
-    int m = tree[id].l + tree[id].r >> 1;
-    if (l <= m) update(id * 2, l, r, k);
-    if (r > m) update(id * 2 + 1, l, r, k);
-    pushup(id);
+    int mid = l + r >> 1;
+    build(x * 2, l, mid);
+    build(x * 2 + 1, mid + 1, r);
+    up(x);
 }
 
-pair<int, int> query(int id, int l, int r) {
-    int res = 0, res2 = 0;
-    if (l <= tree[id].l && tree[id].r <= r) {
+void update(int x, int l, int r, int q, int w) {
+    if (l == r) {
+        tree[x].sum = w;
+        tree[x].sum2 = 1LL * w * w % mod;
+        return;
+    }
+    int mid = l + r >> 1;
+    if (q <= mid)
+        update(x * 2, l, mid, q, w);
+    else
+        update(x * 2 + 1, mid + 1, r, q, w);
+    up(x);
+}
+
+pair<int, int> query(int id, int l, int r, int ql, int qr) {
+    int res1 = 0, res2 = 0;
+    if (ql <= l && r <= qr) {
         return {tree[id].sum, tree[id].sum2};
     }
-    int m = tree[id].l + tree[id].r >> 1;
-    if (l <= m) {
-        auto [x, s2] = query(id * 2, l, r);
-        res = (res + x) % mod;
-        res2 = (res2 + s2) % mod;
+    int mid = l + r >> 1;
+    if (ql <= mid) {
+        auto [x, y] = query(id * 2, l, mid, ql, qr);
+        res1 = (res1 + x) % mod;
+        res2 = (res2 + y) % mod;
     }
-    if (r > m) {
-        auto [x, s2] = query(id * 2 + 1, l, r);
-        res = (res + x) % mod;
-        res2 = (res2 + s2) % mod;
+    if (qr > mid) {
+        auto [x, y] = query(id * 2 + 1, mid + 1, r, ql, qr);
+        res1 = (res1 + x) % mod;
+        res2 = (res2 + y) % mod;
     }
-    return {res, res2};
-}
-
-void build(int id, int l, int r) {
-    tree[id] = {l, r, w[l] % mod, (w[l] % mod) * (w[l] % mod)};
-    if (l == r) return;
-    int m = l + r >> 1;
-    build(id * 2, l, m);
-    build(id * 2 + 1, m + 1, r);
-    pushup(id);
+    return {res1, res2};
 }
 
 signed main() {
@@ -72,24 +77,21 @@ signed main() {
     cin.tie(nullptr);
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
-        cin >> w[i];
+        cin >> b[i];
     build(1, 1, n);
     while (m--) {
-        int op;
-        cin >> op;
-        if (op == 1) {
-            int l, r;
-            cin >> l >> r;
+        int op, l, r;
+        cin >> op >> l >> r;
+        if (op == 1)
+            update(1, 1, n, l, r);
+        else if (op == 2) {
             int len = r - l + 1;
-            int x = query(1, l, r).first;
-            int s2 = query(1, l, r).second;
-            int avg = (x % mod) * ksm(len, mod - 2, mod) % mod;
-            int var = (s2 - ((x * x % mod) * ksm(len, mod - 2, mod)) % mod + mod) % mod;
-            cout << avg << " " << var << "\n";
-        } else if (op == 2) {
-            int k, l;
-            cin >> k >> l;
-            update(1, k, k, l);
+            int x = query(1, 1, n, l, r).first;
+            int y = query(1, 1, n, l, r).second;
+            int inv = ksm(len, mod - 2, mod);
+            int ans =
+                (1LL * (1LL * y * len % mod - 1LL * x * x % mod + mod) * inv % mod * inv) % mod;
+            cout << ans << endl;
         }
     }
     return 0;
