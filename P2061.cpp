9aaef78c008efba45e4126d1b3549747972a9e57
yingMC-1 <19198189385@163.com>
2026-09-19 15:13:49 +0800

    删除 P10096.cpp.bin 和 P1856.cpp.bin 文件；新增 P2061.cpp 文件，包含主要功能实现。

--- P2061.cpp
+++ P2061.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 
-#define int long long
 using namespace std;
-const int N = 1e6 + 5;
+using ll = long long;
+const int N = 4e4 + 5;
 
-int n, k, tot, ans;
+int n, tot;
+ll ans;
 
 struct I {
     int x1, x2, h, w;
@@ -27,7 +28,7 @@ void update(int x, int l, int r, int ql, int qr, int w) {
         push_up(x, l, r);
         return;
     }
-    int mid = l + r >> 1;
+    int mid = (l + r) >> 1;
     if (ql <= mid) update(x * 2, l, mid, ql, qr, w);
     if (qr > mid) update(x * 2 + 1, mid + 1, r, ql, qr, w);
     push_up(x, l, r);
@@ -35,50 +36,28 @@ void update(int x, int l, int r, int ql, int qr, int w) {
 
 bool cmp(I x, I y) { return x.h < y.h; }
 
-signed main() {
-    cin >> k >> n;
-    int newx = 0, newy = 0, lastx = 0, lasty = 0;
+int main() {
+    cin >> n;
     a[0] = {0, 0, 0, 0};
+    int x, y, h;
     for (int i = 1; i <= n; i++) {
-        char c;
-        int x;
-        cin >> c >> x;
-        lastx = newx;
-        lasty = newy;
-        if (c == 'W')
-            newx -= x;
-        else if (c == 'E')
-            newx += x;
-        else if (c == 'N')
-            newy += x;
-        else
-            newy -= x;
-        int x1 = lastx;
-        int x2 = lastx + k;
-        int y1 = lasty;
-        int y2 = lasty + k;
-        if (lastx != newx) {
-            x1 = min(lastx, newx);
-            x2 = max(lastx, newx) + k;
-        } else {
-            y1 = min(lasty, newy);
-            y2 = max(lasty, newy) + k;
-        }
-        a[i] = {x1, x2, y1, 1};
-        a[i + n] = {x1, x2, y2, -1};
+        cin >> x >> y >> h;
+        int x1 = x, x2 = y;
+        a[i] = {x1, x2, 0, 1};
+        a[i + n] = {x1, x2, h, -1};
         b[++tot] = x1;
         b[++tot] = x2;
     }
     sort(b + 1, b + tot + 1);
     tot = unique(b + 1, b + tot + 1) - (b + 1);
-    sort(a + 1, a + n * 2 + 1, cmp);
-    int lastHeight = a[1].h;
+    sort(a + 1, a + tot + 1, cmp);
+    int last = a[1].h;
     for (int i = 1; i <= n * 2; i++) {
         a[i].x1 = lower_bound(b + 1, b + tot + 1, a[i].x1) - b;
         a[i].x2 = lower_bound(b + 1, b + tot + 1, a[i].x2) - b;
-        ans += (a[i].h - lastHeight) * len[1];
+        ans += 1LL * (a[i].h - last) * len[1];
         update(1, 1, tot - 1, a[i].x1, a[i].x2 - 1, a[i].w);
-        lastHeight = a[i].h;
+        last = a[i].h;
     }
     cout << ans << endl;
     return 0;
