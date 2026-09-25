d12b41d76122f28edf0c2029e97f2b6dea656712
yingMC-1 <19198189385@163.com>
2026-09-25 20:34:35 +0800

    Refactor main.cpp to adjust array size and simplify output logic; add new problem statement for "Third Largest Number" with constraints and examples in t.md; remove ege_all_functions.md as it is no longer needed.

--- P11962.cpp
+++ P11962.cpp
@@ -4,8 +4,20 @@ using namespace std;
 using i64 = long long;
 const int N = 2e5 + 5;
 
-int n;
+int n, num[3], vis[N];
 vector<int> g[N];
+
+void dfs(int x, int cnt) {
+    if (cnt % 2 == 0) {
+        num[2]++;
+        vis[x] = 2;
+    } else {
+        num[1]++;
+        vis[x] = 1;
+    }
+    for (int i : g[x])
+        if (!vis[i]) dfs(i, cnt + 1);
+}
 
 int main() {
     cin >> n;
@@ -15,5 +27,8 @@ int main() {
         g[u].push_back(v);
         g[v].push_back(u);
     }
+    dfs(1, 0);
+    for (int i = 1; i <= n; i++)
+        cout << num[vis[i]] << ' ';
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 #define endl '\n'
 using namespace std;
 using i64 = long long;
-const int N = 200000;
+const int N = 5e5 + 5;
 
-int n, q, s, t, a[N];
+int n, a[N];
 
 int main() {
-    cin >> n >> q >> s >> t;
+    cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-    for (int i = 1; i <= q; i++) {
+    sort(a + 1, a + n + 1);
+    for (int i = 1; i <= n - 2; i++) {
+        cout << a[i] << endl;
     }
     return 0;
 }
